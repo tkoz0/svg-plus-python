c0d31f5eca9db04deeacbfb9b6722e214ee04346
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-03-02 06:23:57 -0500

    another one

---
+++ mathcal2025/jan05.py
@@ -0,0 +1,64 @@
+import os
+import sys
+sys.path.insert(0,'..')
+from svgpp import *
+from math import acos, acosh, asin, asinh, atan, atan2, atanh, ceil, comb, \
+    copysign, cos, cosh, degrees, dist, e, erf, erfc, exp, expm1, fabs, \
+    factorial, floor, fmod, frexp, fsum, gamma, gcd, hypot, inf, isclose, \
+    isfinite, isinf, isnan, isqrt, lcm, ldexp, lgamma, log, log10, log1p, \
+    log2, modf, nan, nextafter, perm, pi, pow, prod, radians, remainder, \
+    sin, sinh, sqrt, tan, tanh, tau, trunc, ulp
+from datetime import datetime
+from copy import deepcopy
+SVG_STRS: list[str] = []
+FILE_DATE = os.path.basename(__file__).replace('.py','')
+setwhitespace(4)
+setprefix(f'{FILE_DATE}_')
+
+################################################################################
+# edit here to create svg images and append strings to SVG_STRS
+
+image = svgimage((0,2),(22,14),330,180)
+style_line1 = attrs(stroke='black',stroke_width=0.1,fill='none')
+style_font1 = attrs(fill='black',font_size='0.07em')
+
+image += group([
+    *(rect(x,10,x+2,11) for x in range(1,21,2)),
+    polygon([(11,11),(13,13),(9,13)]),
+    *(rect(x,5,x+2,10) for x in (5,7,15,19))
+],style_line1)
+
+image += group([
+    *(text(f'{w}kg',x-0.25,4) for x,w in ((5,'7'),(7,'5'),(15,'1'),(19,'x')))
+],style_font1)
+
+SVG_STRS.append(str(image))
+
+################################################################################
+# write html file
+
+out = open(f'{FILE_DATE}.html','w')
+header = f'''\
+<!DOCTYPE html>
+<html>
+<head>
+    <style type="text/css">
+        body {{ background: black; }}
+        svg {{ background: white; }}
+    </style>
+</head>
+<body>
+<p style="color:white;">{str(datetime.now())}</p>
+'''
+out.write(header)
+
+for s in SVG_STRS:
+    out.write(f'<hr />\n{s}\n')
+
+footer = f'''\
+</body>
+</html>
+'''
+
+out.write(footer)
+out.close()
